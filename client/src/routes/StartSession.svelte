cd8c43d3ccbdcc2b13dff4ccd24ffb27eb2d7b49
Tyks23 <epic.raider99@gmail.com>
2021-05-13 15:48:27 +0300

    Add Loo Grupp

--- client/src/routes/StartSession.svelte
+++ client/src/routes/StartSession.svelte
@@ -1,5 +1,6 @@
 <script>  
-    import {push} from "svelte-spa-router";
+    import {push, replace} from "svelte-spa-router";
+    
 
     let group = {
         group_name : undefined
@@ -20,9 +21,35 @@
             body: JSON.stringify(group) // body data type must match "Content-Type" header
         });
         if(response.ok){
+           let groupList = document.getElementsByClassName('groupList')[0];
+           let appendable = document.createElement('li');
+           appendable.appendChild(document.createTextNode(group.group_name));
+           groupList.appendChild(appendable);
             //push("/");
         }
     }
+    
+    async function listGroup(){
+        const response = await fetch("http://localhost:3000/listgroup", {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : sessionStorage.getItem('token')
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
+        
+       
+        if(response.ok){
+            return await response.json();
+            //push("/");
+        }
+    };
     </script>
     <div class="container">
         <h1>Harlog</h1>
@@ -33,10 +60,17 @@
         </form>
         <form>
             <p>Vali grupp:</p>
-            <ul>
-                <li><input type="text" bind:value="{group.group_name}"> </li>
-                <li><button on:click|preventDefault="{createGroup}">LOO GRUPP</button></li>
-            </ul>       
+            <ul class = "groupList">
+                {#await listGroup()}
+                <p>Loading groups...</p>
+                {:then groups}
+                {#each groups as group}
+                <li>{group.group_name}</li>
+                {/each}
+                {/await}                
+            </ul>    
+            <input type="text" bind:value="{group.group_name}"> 
+                <button on:click|preventDefault="{createGroup}">LOO GRUPP</button>   
             <button on:click|preventDefault="{() => {push('/activesession')}}">ALUSTA KÜSITLUST</button>
         </form>
     </div>
