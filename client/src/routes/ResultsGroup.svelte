037e2f60ab7b6a06941b89f28a1405befdad59d2
Tyks23 <epic.raider99@gmail.com>
2021-06-14 23:32:35 +0300

    Add more functionality

--- client/src/routes/ResultsGroup.svelte
+++ client/src/routes/ResultsGroup.svelte
@@ -1,54 +1,84 @@
 <script>
-import 'charts.css';
-let selectedGroup = {
-    group_id : sessionStorage.getItem('group_id')
-};
+    import "charts.css";
+    let selectedGroup = {
+        group_id: sessionStorage.getItem("group_id"),
+    };
 
-let activities = {
+    let activities = {};
 
-};
+    let data;
 
-let data;
+    let activityEmotions = {
+        emotion1: 0,
+        emotion2: 0,
+        emotion3: 0,
+    };
 
-let activityEmotions = {
-    emotion1 : 0,
-    emotion2 : 0,
-    emotion3 : 0
-};
+    const getAvgOfCols = (
+        array,
+        start = 0,
+        stop = 0,
+        isReversed = undefined
+    ) => {
+        let counter = 0;
 
-    (async function getActivities(){
+        const newArray = array.reduce((acc, cur) => {
+            counter += stop + 1 - start;
+            return [...acc, ...cur.slice(start, stop + 1)];
+        }, []);
+
+        if (isReversed != undefined)
+            newArray.forEach((x, i) => {
+                if (isReversed[i % isReversed.length]) {
+                    newArray[i] = 6 - x;
+                }
+            });
+
+        return newArray.reduce((acc, cur) => acc + cur, 0) / counter;
+    };
+
+    (async function getActivities() {
         const response = await fetch("http://localhost:3000/getactivities", {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
             headers: {
-            'Content-Type': 'application/json',
-            //'Authorization' : sessionStorage.getItem('token')
-            // 'Content-Type': 'application/x-www-form-urlencoded',
+                "Content-Type": "application/json",
+                //'Authorization' : sessionStorage.getItem('token')
+                // 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(selectedGroup) // body data type must match "Content-Type" header
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(selectedGroup), // body data type must match "Content-Type" header
         });
-        if(response.ok){
+        if (response.ok) {
             data = (await response.json()).rows;
-            
-            activities = Array.from(new Set(data.map((item) => item.activity_id))).reduce((acc, curr) => ({... acc, [curr]: {'name' : '', 'answers' : []}}), {});
-            console.log(activities);
-        data.forEach(row => {
-            console.log(row);
-            activities[row.activity_id].answers.push(row.answer);
-            activities[row.activity_id].name = row.activity_name;
-        
-        });
+
+            activities = Array.from(
+                new Set(data.map((item) => item.activity_id))
+            ).reduce(
+                (acc, curr) => ({ ...acc, [curr]: { name: "", answers: [] } }),
+                {}
+            );
+            data.forEach(({ activity_id, answer, activity_name }) => {
+                //console.log(row);
+                activities[activity_id].answers.push(answer);
+                activities[activity_id].name = activity_name;
+            });
+
             let counter = 0;
-        data.forEach(row => {
-            activityEmotions.emotion1 += activities[activity].answer[0]  + (6 - activities[activity].answer[1]);
-            activityEmotions.emotion2 += row.answer[2] + (6 - row.answer[3]);
-            activityEmotions.emotion3 += row.answer[4] + row.answer[5];
-            counter += 2;
-        });    
+
+            Object.entries(activities).forEach(([, value]) => {
+                const { answers } = value;
+
+                value.emotion1 = getAvgOfCols(answers, 0, 1, [false, true]);
+                value.emotion2 = getAvgOfCols(answers, 2, 3, [false, true]);
+                value.emotion3 = getAvgOfCols(answers, 4, 5);
+            });
+
+            console.log(activities);
+
             console.log(activityEmotions);
             activityEmotions.emotion1 /= counter;
             activityEmotions.emotion2 /= counter;
@@ -56,9 +86,9 @@ let activityEmotions = {
             console.log(activityEmotions);
         }
     })();
-
 </script>
-<div class='container'>
+
+<div class="container">
     <h1>Harlog</h1>
 
     {#each Object.keys(activities) as activity}
@@ -66,42 +96,51 @@ let activityEmotions = {
         <table id="bar-example-6" class="charts-css bar show-labels">
             <caption> Bar Example #6 </caption>
             <thead>
-              <tr>
-                <th scope="col"> Year </th>
-                <th scope="col"> Progress </th>
-              </tr>
+                <tr>
+                    <th scope="col"> Year </th>
+                    <th scope="col"> Progress </th>
+                </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row"> emotsioon 1 </th>
-                <td style="--size:{activityEmotions.emotion1};"><span class="data">{activityEmotions.emotion1}</span></td>
-              </tr>
-              <tr>
-                <th scope="row"> emotsioon 2 </th>
-                <td style="--size:0.4;"></td>
-              </tr>
-              <tr>
-                <th scope="row"> emotsioon 3 </th>
-                <td style="--size:0.6;"></td>
-              </tr>
+                <tr>
+                    <th scope="row"> emot 1 </th>
+                    <td style="--size:{activities[activity].emotion1};">
+                        <span class="data"
+                            >{activities[activity].emotion1.toFixed(2)}</span
+                        >
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"> emot 2 </th>
+                    <td style="--size:{activities[activity].emotion2}">
+                        <span class="data"
+                            >{activities[activity].emotion2.toFixed(2)}</span
+                        >
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"> emot 3 </th>
+                    <td style="--size:{activities[activity].emotion3}">
+                        <span class="data"
+                            >{activities[activity].emotion3.toFixed(2)}</span
+                        >
+                    </td>
+                </tr>
             </tbody>
-          </table>
-        <button></button>
-                              
+        </table>
+        <button />
     {/each}
-
-    
 </div>
 
-
 <style>
-.container {  
-    display: flex;
-    flex-direction: column;
-}
-#bar-example-6 {
-  height: 200px;
-  max-width: 300px;
-  margin: 0 auto;
-}
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+    #bar-example-6 {
+        height: 100px;
+        max-width: 150px;
+        margin: 0 auto;
+        
+    }
 </style>
