b860b2155a3abc308f3edabb1bfa904555197f3b
Tyks23 <epic.raider99@gmail.com>
2021-06-16 22:54:21 +0300

    Switching gameplan!

--- client/src/routes/ComparisonGroup.svelte
+++ client/src/routes/ComparisonGroup.svelte
@@ -9,14 +9,20 @@ import { group_outros } from "svelte/internal";
     let participant1 = false;
     let participant2 = false;
 
-    let selectedGroup1 = {};
-    let selectedActivity1 = {};
-    let selectedParticipant1 = {};
+    let data;
+
+    let selected1 = {
+        group_id : null, 
+        activity_id : null,
+        part_id : null
+    };
 
 
-    let selectedGroup2 = {};
-    let selectedActivity2 = {};
-    let selectedParticipant2 = {};
+    let selected2 = {
+        group_id : null, 
+        activity_id : null,
+        part_id : null
+    };
 
     async function listGroup(){
         const response = await fetch("http://localhost:3000/listgroup", {
@@ -76,14 +82,66 @@ import { group_outros } from "svelte/internal";
             return await response.json();
             //push("/");
         }};
-    
+
+
+    let activityEmotions = {
+        emotion1: 0,
+        emotion2: 0,
+        emotion3: 0,
+    };
+    const getAvgOfCols = (
+        array,
+        start = 0,
+        stop = 0,
+        isReversed = undefined
+    ) => {
+        let counter = 0;
+        const newArray = array.reduce((acc, cur) => {
+            counter += stop + 1 - start;
+            return [...acc, ...cur.slice(start, stop + 1)];
+        }, []);
+        if (isReversed != undefined)
+            newArray.forEach((x, i) => {
+                if (isReversed[i % isReversed.length]) {
+                    newArray[i] = 6 - x;
+                }
+            });
+
+        return newArray.reduce((acc, cur) => acc + cur, 0) / counter;
+    };
+
+    async function getComparisonData(input){
+
+        const parsed = Object.entries(input).reduce((acc, [key, value]) => {
+        if (value !== null) return { ...acc, [key]: value };
+        return acc;
+        }, {});
+
+        const response = await fetch("http://localhost:3000/getcomparisondata", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(parsed)
+        });
+        if (response.ok) {
+            
+           
+    }
+}
 </script>
 <div class="container">
     <form>
         <label for="groups">Vali esimene grupp</label>
         <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selectedGroup1.group_id}" on:change="{() => {activity1 = true;}}">
-            <option>Select group</option>
+        <select bind:value="{selected1.group_id}" on:change="{() => {activity1 = true;}}">
+            <option value="{null}">Select group</option>
             {#await listGroup()}
             <p>Loading groups...</p>
             {:then groups}
@@ -94,9 +152,9 @@ import { group_outros } from "svelte/internal";
         </select>
         {#if activity1}
         <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selectedActivity1.activity_id}" on:change="{() => {participant1 = true;}}">
-            <option>Select activity</option>
-            {#await listActivities(selectedGroup1)}
+        <select bind:value="{selected1.activity_id}" on:change="{() => {participant1 = true;}}">
+            <option value="{null}">Select activity</option>
+            {#await listActivities(selected1)}
             <p>Loading activities...</p>
             {:then activities}
             {#each activities as activity}      
@@ -106,9 +164,9 @@ import { group_outros } from "svelte/internal";
         </select>    
         {/if}    
         {#if participant1}
-        <select bind:value="{selectedParticipant1.part_id}">
-            <option>Select participant </option>
-            {#await listParticipants(selectedActivity1)}
+        <select bind:value="{selected1.part_id}">
+            <option value="{null}">Select participant </option>
+            {#await listParticipants(selected1)}
             <p>Loading participants...</p>
             {:then participants}
             {#each participants as participant}      
@@ -118,12 +176,12 @@ import { group_outros } from "svelte/internal";
         </select>
         {/if}
     </form>
-    <button on:click="{() => {/*window.location.reload()*/console.log(selectedGroup1)}}" />
+    <button on:click="{() => {/*window.location.reload()*/console.log(selected1)}}" />
     <form>
         <label for="groups">Vali teine grupp</label>
         <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selectedGroup2.group_id}" on:change="{() => {activity2 = true;}}">
-            <option>Select group</option>
+        <select bind:value="{selected2.group_id}" on:change="{() => {activity2 = true;}}">
+            <option value="{null}">Select group</option>
             {#await listGroup()}
             <p>Loading groups...</p>
             {:then groups}
@@ -134,9 +192,9 @@ import { group_outros } from "svelte/internal";
         </select>
         {#if activity2}
         <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selectedActivity2.activity_id}" on:change="{() => {participant2 = true;}}">
-            <option>Select activity</option>
-            {#await listActivities()}
+        <select bind:value="{selected2.activity_id}"  on:change="{() => {participant2 = true;}}">
+            <option value="{null}" disabled>Select activity</option>
+            {#await listActivities(selected2)}
             <p>Loading activities...</p>
             {:then activities}
             {#each activities as activity}      
@@ -146,9 +204,9 @@ import { group_outros } from "svelte/internal";
         </select>    
         {/if}    
         {#if participant2}
-        <select bind:value="{selectedParticipant2.activity_id}">
-            <option>Select participant </option>
-            {#await listParticipants()}
+        <select bind:value="{selected2.part_id}">
+            <option value="{null}">Select participant </option>
+            {#await listParticipants(selected2)}
             <p>Loading participants...</p>
             {:then participants}
             {#each participants as participant}      
@@ -158,6 +216,7 @@ import { group_outros } from "svelte/internal";
         </select>
         {/if}
     </form>
+    <button on:click="{() => {getComparisonData(selected1)}}" />
 </div>
 <style>
 
