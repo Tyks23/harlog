45658343f8937c68aa45a61ccb95bddc146d0505
Tyks23 <epic.raider99@gmail.com>
2021-06-16 23:39:28 +0300

    Add pekki valemid

--- client/src/routes/ComparisonGroup.svelte
+++ client/src/routes/ComparisonGroup.svelte
@@ -2,12 +2,10 @@
 import { group_outros } from "svelte/internal";
 
 
-    
-    let activity1 = false;
-    let activity2 = false;
+    let participants = {};    
 
+    let activity1 = false;
     let participant1 = false;
-    let participant2 = false;
 
     let data;
 
@@ -17,12 +15,6 @@ import { group_outros } from "svelte/internal";
         part_id : null
     };
 
-
-    let selected2 = {
-        group_id : null, 
-        activity_id : null,
-        part_id : null
-    };
 
     async function listGroup(){
         const response = await fetch("http://localhost:3000/listgroup", {
@@ -131,10 +123,60 @@ import { group_outros } from "svelte/internal";
             body: JSON.stringify(parsed)
         });
         if (response.ok) {
-            
-           
+            data = (await response.json()).rows;
+            console.log(data);
+
+            if(participant1 && activity1){
+                participants = Array.from(
+                new Set(data.map((item) => item.part_id))
+            ).reduce(
+                (acc, curr) => ({ ...acc, [curr]: { name: "", answers: [] } }),
+                {}
+            );
+            data.forEach(({ part_id, answer, part_name }) => {
+                //console.log(row);
+                participants[part_id].answers.push(answer);
+                participants[part_id].name = part_name;
+            });
+
+            let counter = 0;
+
+            Object.entries(participants).forEach(([, value]) => {
+                const { answers } = value;
+
+                value.emotion1 = getAvgOfCols(answers, 0, 1, [false, true]);
+                value.emotion2 = getAvgOfCols(answers, 2, 3, [false, true]);
+                value.emotion3 = getAvgOfCols(answers, 4, 5);
+
+                activityEmotions.emotion1 = value.emotion1;
+            });
+            }
+            if(activity1 && !participant1){
+                new Set(data.map((item) => item.activity_id))
+            ).reduce(
+                (acc, curr) => ({ ...acc, [curr]: { name: "", answers: [] } }),
+                {}
+            );
+            data.forEach(({ activity_id, answer, activity_name }) => {
+                activities[activity_id].answers.push(answer);
+                activities[activity_id].name = activity_name;
+            });
+
+            let counter = 0;
+
+            Object.entries(activities).forEach(([, value]) => {
+                const { answers } = value;
+
+                value.emotion1 = getAvgOfCols(answers, 0, 1, [false, true]);
+                value.emotion2 = getAvgOfCols(answers, 2, 3, [false, true]);
+                value.emotion3 = getAvgOfCols(answers, 4, 5);
+            });
+            }
+            if(!activity1 && !participant1){
+                
+            }
+        }
     }
-}
 </script>
 <div class="container">
     <form>
@@ -177,47 +219,28 @@ import { group_outros } from "svelte/internal";
         {/if}
     </form>
     <button on:click="{() => {/*window.location.reload()*/console.log(selected1)}}" />
-    <form>
-        <label for="groups">Vali teine grupp</label>
-        <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selected2.group_id}" on:change="{() => {activity2 = true;}}">
-            <option value="{null}">Select group</option>
-            {#await listGroup()}
-            <p>Loading groups...</p>
-            {:then groups}
-            {#each groups as group}      
-            <option value="{group.group_id}">{group.group_name}</option>
-            {/each}
-            {/await}                
-        </select>
-        {#if activity2}
-        <!-- svelte-ignore a11y-no-onchange -->
-        <select bind:value="{selected2.activity_id}"  on:change="{() => {participant2 = true;}}">
-            <option value="{null}" disabled>Select activity</option>
-            {#await listActivities(selected2)}
-            <p>Loading activities...</p>
-            {:then activities}
-            {#each activities as activity}      
-            <option value="{activity.activity_id}">{activity.activity_name}</option>
-            {/each}
-            {/await}                
-        </select>    
-        {/if}    
-        {#if participant2}
-        <select bind:value="{selected2.part_id}">
-            <option value="{null}">Select participant </option>
-            {#await listParticipants(selected2)}
-            <p>Loading participants...</p>
-            {:then participants}
-            {#each participants as participant}      
-            <option value="{participant.part_id}">{participant.part_name}</option>
-            {/each}
-            {/await}                
-        </select>
-        {/if}
-    </form>
     <button on:click="{() => {getComparisonData(selected1)}}" />
 </div>
+<div>
+    <table id="column-example-5" class="charts-css column multiple data-spacing-3">
+        <caption> Vordlused </caption>
+        <tbody>
+          <tr>
+            <td style="--size:{activityEmotions.emotion1}"></td>
+            <td style="--size:{activityEmotions.emotion2}"></td>
+            <td style="--size:{activityEmotions.emotion3}"></td>
+          </tr>
+          <tr>
+            
+          </tr>
+        </tbody>
+      </table>
+</div>
 <style>
+    #column-example-5 {
+  height: 200px;
+  max-width: 300px;
+  margin: 0 auto;
+}
 
 </style>
